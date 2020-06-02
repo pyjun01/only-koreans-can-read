72e691f016aff7ffd2653ed032914ac34f2de932
pyjun01 <pyjun01@naver.com>
2020-06-03 00:12:14 +0900

    Updates

---
+++ precache-manifest.65a101a3d20569643b931073e50852c0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c10f72ec2fb44eaff26e6e66313d542",
+    "url": "/onlyKoreansRead/index.html"
+  },
+  {
+    "revision": "2268afb6662de3374052",
+    "url": "/onlyKoreansRead/static/js/2.a3a57329.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/onlyKoreansRead/static/js/2.a3a57329.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "62aad0d0f7371791c927",
+    "url": "/onlyKoreansRead/static/js/main.1c1c1a4a.chunk.js"
+  },
+  {
+    "revision": "136814b103572b42a403",
+    "url": "/onlyKoreansRead/static/js/runtime-main.33f54942.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/onlyKoreansRead/static/media/logo.5d5d9eef.svg"
+  }
+]);
